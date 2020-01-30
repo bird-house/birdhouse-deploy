91d711d1732ccf71f33e945273595ea0ec376965
David Caron <david.caron@crim.ca>
2020-01-30 15:07:41 -0500

    undo backup-datavolume.sh modifications

--- birdhouse/scripts/backup-datavolume.sh
+++ birdhouse/scripts/backup-datavolume.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -x
-# Backup DATA_VOL_DIR to BACKUP_OUT_DIR/DATA_VOL_DIR.tgz
+# Backup DATA_VOL_NAME to BACKUP_OUT_DIR/DATA_VOL_NAME.tgz
 
-DATA_VOL_DIR="$1"; shift
+DATA_VOL_NAME="$1"; shift
 BACKUP_OUT_DIR="$1"; shift
 
 docker run --rm \
-  --name backup_data_volume \
+  --name backup_data_vol_$DATA_VOL_NAME \
   -u root \
   -v $BACKUP_OUT_DIR:/backups \
-  -v $DATA_VOL_DIR:/data_vol_to_backup:ro \
+  -v $DATA_VOL_NAME:/data_vol_to_backup:ro \
   bash \
-  tar czvf /backups/data_volume_backup.tgz -C /data_vol_to_backup .
+  tar czvf /backups/$DATA_VOL_NAME.tgz -C /data_vol_to_backup .
 
 # vi: tabstop=8 expandtab shiftwidth=4 softtabstop=4
