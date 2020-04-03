66f7a9d40747f3a3178b286b975e8817864ff9bf
Long Vu <vu.long@ouranos.ca>
2020-04-03 17:24:37 -0400

    vagrant: deactivate old autodeploy install scripts in favor of the new ./component/scheduler

--- birdhouse/vagrant-utils/provision.sh
+++ birdhouse/vagrant-utils/provision.sh
@@ -4,5 +4,3 @@ cd /vagrant/birdhouse
 vagrant-utils/install-docker.sh
 vagrant-utils/configure-pavics.sh
 vagrant-utils/configure-pagekite
-deployment/install-deploy-notebook /vagrant
-deployment/install-logrotate-config /vagrant vagrant
