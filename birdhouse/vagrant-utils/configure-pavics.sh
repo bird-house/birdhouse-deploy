c127a6742a8b10be2c712b189b938deb3ab87b99
Long Vu <vu.long@ouranos.ca>
2020-09-10 10:58:23 -0400

    deploy: make auto-renew LetsEncrypt SSL cronjob configurable

--- birdhouse/vagrant-utils/configure-pavics.sh
+++ birdhouse/vagrant-utils/configure-pavics.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -x
 
+if [ -z "$SSL_CERTIFICATE" ]; then
+    # Overridable
+    SSL_CERTIFICATE="/home/vagrant/certkey.pem"
+fi
+
 if [ ! -f env.local ]; then
     cp env.local.example env.local
     cat <<EOF >> env.local
 
 # override with values needed for vagrant
-export SSL_CERTIFICATE='./certkey.pem'  # path to the nginx ssl certificate, path and key bundle
+export SSL_CERTIFICATE='$SSL_CERTIFICATE'  # *absolute* path to the nginx ssl certificate, path and key bundle
 export PAVICS_FQDN='${VM_HOSTNAME}.$VM_DOMAIN' # Fully qualified domain name of this Pavics installation
 EOF
 
@@ -24,7 +29,7 @@ else
     echo "existing env.local file, not overriding"
 fi
 
-if [ ! -f certkey.pem ]; then
+if [ ! -f "$SSL_CERTIFICATE" ]; then
     . ./env.local
     if [ -n "$LETSENCRYPT_EMAIL" ]; then
         FORCE_CERTBOT_E2E=1 FORCE_CERTBOT_E2E_NO_START_PROXY=1 \
@@ -32,8 +37,8 @@ if [ ! -f certkey.pem ]; then
     else
         openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem \
             -subj "/C=CA/ST=Quebec/L=Montreal/O=RnD/CN=${VM_HOSTNAME}.$VM_DOMAIN"
-        cp cert.pem certkey.pem
-        cat key.pem >> certkey.pem
+        cp cert.pem "$SSL_CERTIFICATE"
+        cat key.pem >> "$SSL_CERTIFICATE"
         if [ -z "$VERIFY_SSL" ]; then
             cat <<EOF >> env.local
 export VERIFY_SSL="false"
@@ -41,7 +46,7 @@ EOF
         fi
     fi
 else
-    echo "existing certkey.pem file, not overriding"
+    echo "existing '$SSL_CERTIFICATE' file, not overriding"
 fi
 
 ./pavics-compose.sh up -d
