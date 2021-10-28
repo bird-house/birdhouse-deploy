9a42ffce0ee8ffdfa9390f29645d3cbe8c5fe304
Long Vu <vu.long@ouranos.ca>
2021-10-28 15:09:18 -0400

    finch: ensure workdir also on same partion as outputdir for hardlink to work

--- birdhouse/pavics-compose.sh
+++ birdhouse/pavics-compose.sh
@@ -204,7 +204,7 @@ do
 
     # because server.outputpath in wps.cfg do not create the dir
     for bird in finch flyingpigeon raven; do
-      docker exec $bird mkdir -p /data/wpsoutputs/$bird/tmp
+      docker exec $bird mkdir -p /data/wpsoutputs/$bird/tmp /data/wpsoutputs/$bird/workdir
     done
 
     for adir in ${EXTRA_CONF_DIRS}; do
