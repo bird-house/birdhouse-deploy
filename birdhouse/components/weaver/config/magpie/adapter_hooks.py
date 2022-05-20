e1c3f736a4bb2da38e6fe940fdfe4f455a39f958
Francis Charette-Migneault <francis.charette.migneault@gmail.com>
2022-05-20 17:14:42 -0400

    fix invalid paths + remove test code + add more details/case handling

--- birdhouse/components/weaver/config/magpie/adapter_hooks.py
+++ birdhouse/components/weaver/config/magpie/adapter_hooks.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import json
+"""
+These hooks will be running within Twitcher, using MagpieAdapter context.
+
+The code below can make use of any package that is installed by Magpie/Twitcher.
+"""
+
 from typing import TYPE_CHECKING
+
+from magpie.constants import get_constant
+from magpie.utils import get_header
 
 if TYPE_CHECKING:
     from pyramid.request import Request
 
-    from magpie.typedefs import ServiceConfigItem
+
+def is_admin(request):
+    # type: (Request) -> bool
+    admin_group = get_constant("MAGPIE_ADMIN_GROUP", settings_container=request)
+    return admin_group in [group.group_name for group in request.user.groups]
 
 
-def add_x_wps_output_context(request, service):
-    # type: (Request, ServiceConfigItem) -> Request
-    if "application/json" in request.content_type:
-        body = request.json  # JSON generated from body, cannot override directly
-        # following for testing purposes only
-        body["hooks"] = len(service["hooks"])
-        body["hook"] = "add_x_wps_output_context"
-        request.body = json.dumps(body).encode()
-    if request.user is not None:
-        request.headers["X-WPS-Output-Context"] = "user-" + str(request.user.id)
+def add_x_wps_output_context(request):
+    # type: (Request) -> Request
+    """
+    Apply the ``X-WPS-Output-Context`` for saving outputs in the user-context WPS-outputs directory.
+    """
+    header = get_header("X-WPS-Output-Context", request.headers)
+    # if explicitly provided, ensure it is permitted (admin allow any, otherwise self-user reference only)
+    if header is not None:
+        if request.user is None:
+            header = "public"
+        else:
+            if not is_admin(request):
+                # override disallowed writing to other location
+                # otherwise, up to admin to have writen something sensible
+                header = "user-" + str(request.user.id)
+    else:
+        if request.user is None:
+            header = "public"
+        else:
+            header = "user-" + str(request.user.id)
+    request.headers["X-WPS-Output-Context"] = header
     return request
