394b36f1236873b7ad9ac128a7796f5b0206c15e
Mathieu Provencher <>
2023-02-22 23:50:43 -0500

    working asset generator timeout

--- birdhouse/components/stac/populator/populate.sh
+++ birdhouse/components/stac/populator/populate.sh
@@ -2,8 +2,10 @@
 set -e
 
 BASE_PATH=""
-STAC_ASSET_GENERATOR_TIMEOUT=3
+STAC_ASSET_GENERATOR_TIMEOUT=10
 STAC_HOST=https://host-140-133.rdext.crim.ca/stac
+
+cd $BASE_PATH/populator
 
 # create collections
 python3 ./collection_processor.py collections.yaml
@@ -13,12 +15,11 @@ cd $BASE_PATH/stac-generator-example
 # replace STAC host name of asset generators
 sed -i "/host:/c\      host: $STAC_HOST" conf/thredds-cmip6-asset-generator.yaml
 sed -i "/host:/c\      host: $STAC_HOST" conf/thredds-cmip5-asset-generator.yaml
-cat conf/thredds-cmip5-asset-generator.yaml
 
 # add items
-timeout -s STOP $STAC_ASSET_GENERATOR_TIMEOUT bash -c "python3 -m stac_generator.scripts.stac_generator conf/thredds-cmip6-asset-generator.yaml" &
+timeout $STAC_ASSET_GENERATOR_TIMEOUT bash -c "python3 -m stac_generator.scripts.stac_generator conf/thredds-cmip6-asset-generator.yaml" || true &
 PID_CMIP6=$!
-timeout -s STOP $STAC_ASSET_GENERATOR_TIMEOUT bash -c "python3 -m stac_generator.scripts.stac_generator conf/thredds-cmip5-asset-generator.yaml" &
+timeout $STAC_ASSET_GENERATOR_TIMEOUT bash -c "python3 -m stac_generator.scripts.stac_generator conf/thredds-cmip5-asset-generator.yaml" || true &
 PID_CMIP5=$!
 
 # kill the asset generators if CTRL-C is sent
@@ -28,9 +29,9 @@ echo "Running STAC asset generator for $STAC_ASSET_GENERATOR_TIMEOUT seconds..."
 wait $PID_CMIP6
 wait $PID_CMIP5
 
-# cd $BASE_PATH/
+cd $BASE_PATH/populator
 
-# # update collection summaries
-# python3 collection_processor.py collections.yaml
+# update collection summaries
+python3 collection_processor.py collections.yaml
 
-# echo "STAC asset generator ran for $STAC_ASSET_GENERATOR_TIMEOUT seconds. Exiting."
+echo "STAC asset generator ran for $STAC_ASSET_GENERATOR_TIMEOUT seconds. Exiting."
