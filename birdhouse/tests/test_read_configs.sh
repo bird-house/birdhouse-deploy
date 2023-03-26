778c2a1109fb236c81251346cbab14f83c2c8d55
Long Vu <vu.long@ouranos.ca>
2023-03-25 23:06:42 -0400

    tests: add simple test for COMPOSE_CONF_LIST
    Had to change test PWD to same as COMPOSE_DIR to be able to discover all
    component/config/*/docker-compose-extra.yml.
    
    ```
    $ ./bash_unit test_read_configs.sh
    Running tests in test_read_configs.sh
            Running test_simple_all_conf_dirs ... SUCCESS ✓
            Running test_simple_compose_conf_list ... SUCCESS ✓
    Overall result: SUCCESS ✓
    ```

--- birdhouse/tests/test_read_configs.sh
+++ birdhouse/tests/test_read_configs.sh
@@ -1,12 +1,15 @@
 setup_suite() {
   if [ ! -f ../read-configs.include.sh ]; then
-    echo "ERROR: expected current dir is test dir." 1>&2
+    echo "ERROR: expected initial current dir is test dir." 1>&2
     exit 2
   fi
 
-  . ../read-configs.include.sh
+  # Set test workdir to be COMPOSE_DIR to emulate normal run of ./pavics-compose.sh.
+  # Needed to discover component/config/*/docker-compose.yml for COMPOSE_CONF_LIST.
+  cd ..
+  . ./read-configs.include.sh
 
-  if [ -e "../env.local" ]; then
+  if [ -e "./env.local" ]; then
       echo "ERROR: please save your env.local to avoid losing it.  We will override it with our test version." 1>&2
       exit 2
   fi
@@ -14,25 +17,30 @@ setup_suite() {
 
 
 teardown_suite() {
-    rm ../env.local
+    rm ./env.local
 }
 
 
-test_simple_all_conf_dirs_content() {
-
-    # Simple env.local.
+gen_simple_env_local() {
     echo "
 export EXTRA_CONF_DIRS='
     ./optional-components/all-public-access
     ./optional-components/test-weaver
-'" > ../env.local
+'" > ./env.local
+}
+
+
+test_simple_all_conf_dirs() {
+
+    gen_simple_env_local
 
     # Redirect STDOUT to /dev/null to hide all "reading './config/XYX/default.env'" and
     # "delayed eval 'VAR=value'" messages.
     read_configs > /dev/null
 
-    # TODO: expected wrong output just to get test to pass.
+    # TODO: expected is wrong output just to get test to pass.
     # test-weaver depends on weaver so weaver has to be listed first.
+
     EXPECTED_ALL_CONF_DIRS="
             ./config/proxy
           
@@ -88,4 +96,94 @@ export EXTRA_CONF_DIRS='
           "
 
     assert_equals "$EXPECTED_ALL_CONF_DIRS" "$ALL_CONF_DIRS"
+}
+
+
+test_simple_compose_conf_list() {
+
+    gen_simple_env_local
+
+    # Redirect STDOUT to /dev/null to hide all "reading './config/XYX/default.env'" and
+    # "delayed eval 'VAR=value'" messages.
+    read_configs > /dev/null
+
+    gen_compose_conf_list
+
+    # TODO: expected output is wrong, just to pass the test.
+    # Last entry should have been ./optional-components/test-weaver/docker-compose-extra.yml
+    # because it is the last item of EXTRA_CONF_DIRS.
+    # But we see a bunch of ./config/{component}/config/*/docker-compose-extra.yml after.
+
+    EXPECTED_COMPOSE_CONF_LIST="
+-f docker-compose.yml 
+-f ./config/proxy/docker-compose-extra.yml 
+-f ./config/phoenix/docker-compose-extra.yml 
+-f ./config/malleefowl/docker-compose-extra.yml 
+-f ./config/postgres/docker-compose-extra.yml 
+-f ./config/wps_outputs-volume/docker-compose-extra.yml 
+-f ./config/data-volume/docker-compose-extra.yml 
+-f ./config/flyingpigeon/docker-compose-extra.yml 
+-f ./config/catalog/docker-compose-extra.yml 
+-f ./config/mongodb/docker-compose-extra.yml 
+-f ./config/geoserver/docker-compose-extra.yml 
+-f ./config/finch/docker-compose-extra.yml 
+-f ./config/raven/docker-compose-extra.yml 
+-f ./config/hummingbird/docker-compose-extra.yml 
+-f ./config/thredds/docker-compose-extra.yml 
+-f ./config/portainer/docker-compose-extra.yml 
+-f ./config/magpie/docker-compose-extra.yml 
+-f ./config/twitcher/docker-compose-extra.yml 
+-f ./config/jupyterhub/docker-compose-extra.yml 
+-f ./config/frontend/docker-compose-extra.yml 
+-f ./config/ncwms2/docker-compose-extra.yml 
+-f ./config/project-api/docker-compose-extra.yml 
+-f ./config/solr/docker-compose-extra.yml 
+-f ./optional-components/test-weaver/docker-compose-extra.yml 
+-f ./components/weaver/docker-compose-extra.yml 
+-f ./config/canarie-api/config/proxy/docker-compose-extra.yml 
+-f ./config/phoenix/config/proxy/docker-compose-extra.yml 
+-f ./config/malleefowl/config/data-volume/docker-compose-extra.yml 
+-f ./config/malleefowl/config/magpie/docker-compose-extra.yml 
+-f ./config/malleefowl/config/proxy/docker-compose-extra.yml 
+-f ./config/malleefowl/config/wps_outputs-volume/docker-compose-extra.yml 
+-f ./config/wps_outputs-volume/config/proxy/docker-compose-extra.yml 
+-f ./config/flyingpigeon/config/magpie/docker-compose-extra.yml 
+-f ./config/flyingpigeon/config/wps_outputs-volume/docker-compose-extra.yml 
+-f ./config/catalog/config/magpie/docker-compose-extra.yml 
+-f ./config/catalog/config/proxy/docker-compose-extra.yml 
+-f ./config/geoserver/config/magpie/docker-compose-extra.yml 
+-f ./config/geoserver/config/proxy/docker-compose-extra.yml 
+-f ./config/finch/config/magpie/docker-compose-extra.yml 
+-f ./config/finch/config/proxy/docker-compose-extra.yml 
+-f ./config/finch/config/wps_outputs-volume/docker-compose-extra.yml 
+-f ./config/raven/config/magpie/docker-compose-extra.yml 
+-f ./config/raven/config/proxy/docker-compose-extra.yml 
+-f ./config/raven/config/wps_outputs-volume/docker-compose-extra.yml 
+-f ./config/hummingbird/config/data-volume/docker-compose-extra.yml 
+-f ./config/hummingbird/config/magpie/docker-compose-extra.yml 
+-f ./config/hummingbird/config/wps_outputs-volume/docker-compose-extra.yml 
+-f ./config/thredds/config/proxy/docker-compose-extra.yml 
+-f ./config/portainer/config/proxy/docker-compose-extra.yml 
+-f ./config/magpie/config/proxy/docker-compose-extra.yml 
+-f ./config/twitcher/config/proxy/docker-compose-extra.yml 
+-f ./config/jupyterhub/config/magpie/docker-compose-extra.yml 
+-f ./config/jupyterhub/config/proxy/docker-compose-extra.yml 
+-f ./config/frontend/config/proxy/docker-compose-extra.yml 
+-f ./config/ncwms2/config/magpie/docker-compose-extra.yml 
+-f ./config/ncwms2/config/proxy/docker-compose-extra.yml 
+-f ./config/ncwms2/config/wps_outputs-volume/docker-compose-extra.yml 
+-f ./config/project-api/config/proxy/docker-compose-extra.yml 
+-f ./config/solr/config/proxy/docker-compose-extra.yml 
+-f ./optional-components/all-public-access/config/catalog/docker-compose-extra.yml 
+-f ./optional-components/all-public-access/config/finch/docker-compose-extra.yml 
+-f ./optional-components/all-public-access/config/flyingpigeon/docker-compose-extra.yml 
+-f ./optional-components/all-public-access/config/hummingbird/docker-compose-extra.yml 
+-f ./optional-components/all-public-access/config/malleefowl/docker-compose-extra.yml 
+-f ./optional-components/all-public-access/config/raven/docker-compose-extra.yml 
+-f ./optional-components/all-public-access/config/thredds/docker-compose-extra.yml 
+-f ./components/weaver/config/magpie/docker-compose-extra.yml 
+-f ./components/weaver/config/proxy/docker-compose-extra.yml 
+-f ./components/weaver/config/twitcher/docker-compose-extra.yml"
+
+    assert_equals "$EXPECTED_COMPOSE_CONF_LIST" "$(echo "$COMPOSE_CONF_LIST" | sed 's/-f /\n-f /g')"
 }
