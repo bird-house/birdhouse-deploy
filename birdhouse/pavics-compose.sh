d58d79a1d35a49322e4cd4a732cf6867854e6d32
mishaschwartz <misha.schwartz@gmail.com>
2023-06-05 11:45:18 -0400

    convert version endpoint to display version and release time as json string

--- birdhouse/pavics-compose.sh
+++ birdhouse/pavics-compose.sh
@@ -50,7 +50,7 @@ read_configs # this sets ALL_CONF_DIRS
 
 . ./scripts/get-components-json.include.sh
 . ./scripts/get-services-json.include.sh
-. ./scripts/get-version.include.sh
+. ./scripts/get-version-json.include.sh
 
 for i in ${VARS}
 do
