4d0efad247f1d8df9b28da40f4e67744756c943f
mishaschwartz <misha.schwartz@gmail.com>
2023-06-12 09:49:24 -0400

    serve services, components, and version information from a file

--- birdhouse/scripts/get-services-json.include.sh
+++ birdhouse/scripts/get-services-json.include.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 # default value in case of error or missing definitions
-export BIRDHOUSE_DEPLOY_SERVICES_JSON='{}'
-if [ -z "${ALL_CONF_DIRS}" ]; then
-  echo "No services in DEFAULT_CONF_DIRS and EXTRA_CONF_DIRS. SERVICES JSON list will be empty!"
-  return
-fi
 
 for adir in ${ALL_CONF_DIRS}; do
   [ -f "${adir}/service-config.json" ] || continue
   SERVICES="${SERVICES}$([ -n "${SERVICES}" ] && echo ',') $(cat "${adir}/service-config.json")"
 done
 
+if [ -z "${SERVICES}" ]; then
+  echo "No services in DEFAULT_CONF_DIRS and EXTRA_CONF_DIRS. SERVICES JSON list will be empty!"
+fi
 export BIRDHOUSE_DEPLOY_SERVICES_JSON="{\"services\": [${SERVICES}]}"
