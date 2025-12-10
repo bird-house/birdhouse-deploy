e025b87253e2f3e1e0f783550b8bfac48fb3e223
Long Vu <vu.long@ouranos.ca>
2025-12-10 13:45:50 -0500

    proxy: fix logrotate cron job do not fire

--- birdhouse/components/proxy/nginx-docker-entrypoint.d/launch_cron.sh
+++ birdhouse/components/proxy/nginx-docker-entrypoint.d/launch_cron.sh
@@ -1,3 +1,17 @@
 #!/bin/sh -x
 
+# Those config files need to have proper ownership and permissions, else the
+# deamon will not run.
+deploy_config() {
+    srcfile="$1"
+    destfile="$2"
+    cp -v "$srcfile" "$destfile"
+    chown root:root "$destfile"
+    chmod 644 "$destfile"
+}
+
+
+deploy_config /file/cron.d/logrotate /etc/cron.d/logrotate
+deploy_config /file/logrotate.d/nginx /etc/logrotate.d/nginx
+
 cron
